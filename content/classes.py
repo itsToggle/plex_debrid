06a3e4b9798c3a1341dbc34bf333ce8cd553ef6f
itsToggle <71379623+itsToggle@users.noreply.github.com>
2023-02-01 11:25:22 +0100

    add airtime offset trigger #373

--- content/classes.py
+++ content/classes.py
@@ -617,20 +617,19 @@ class media:
 
     def released(self):
         try:
+            for version in self.versions():
+                for i,trigger in enumerate(version.triggers):
+                    if trigger[0] == "airtime offset":
+                        self.originallyAvailableAt = datetime.datetime.strptime(self.originallyAvailableAt,'%Y-%m-%d') + datetime.timedelta(hours=float(trigger[2]))
+                        self.originallyAvailableAt = self.originallyAvailableAt.strftime('%Y-%m-%d')
             released = datetime.datetime.today() - datetime.datetime.strptime(self.originallyAvailableAt,'%Y-%m-%d')
             if self.type == 'movie':
                 if released.days >= -30 and released.days <= 60:
-                    if self.available():
-                        return True
-                    else:
-                        return False
+                    return self.available()
                 return released.days > 0
             else:
                 if released.days >= -1 and released.days <= 1:
-                    if self.available():
-                        return True
-                    else:
-                        return False
+                    return self.available()
                 return released.days >= 0
         except Exception as e:
             ui_print("media error: (attr exception): " + str(e), debug=ui_settings.debug)
@@ -643,12 +642,18 @@ class media:
         if (self.watchlist == plex.watchlist and len(trakt.users) > 0) or self.watchlist == trakt.watchlist:
             if self.watchlist == plex.watchlist:
                 self.match('content.services.trakt')
+            offset = "0"
+            for version in self.versions():
+                for i,trigger in enumerate(version.triggers):
+                    if trigger[0] == "airtime offset":
+                        offset = trigger[2]
+            released = datetime.datetime.today() - datetime.datetime.strptime(self.originallyAvailableAt,'%Y-%m-%d')
             trakt_match = self
             if not trakt_match == None:
                 trakt.current_user = trakt.users[0]
                 try:
                     if trakt_match.type == 'show':
-                        return datetime.datetime.utcnow() > datetime.datetime.strptime(trakt_match.first_aired,'%Y-%m-%dT%H:%M:%S.000Z')
+                        return datetime.datetime.utcnow() > datetime.datetime.strptime(trakt_match.first_aired,'%Y-%m-%dT%H:%M:%S.000Z') + datetime.timedelta(hours=float(offset))
                     elif trakt_match.type == 'movie':
                         release_date = None
                         releases, header = trakt.get(
@@ -677,14 +682,14 @@ class media:
                                 if regex.search(r'(latest|new).*?(releases)', trakt_list.name, regex.I):
                                     match = True
                         # if release_date and delay have passed or the movie was released early
-                        return datetime.datetime.utcnow() > datetime.datetime.strptime(release_date,'%Y-%m-%d') or match
+                        return datetime.datetime.utcnow() > datetime.datetime.strptime(release_date,'%Y-%m-%d')  + datetime.timedelta(hours=float(offset)) or match
                     elif trakt_match.type == 'season':
                         try:
-                            return datetime.datetime.utcnow() > datetime.datetime.strptime(trakt_match.first_aired,'%Y-%m-%dT%H:%M:%S.000Z')
+                            return datetime.datetime.utcnow() > datetime.datetime.strptime(trakt_match.first_aired,'%Y-%m-%dT%H:%M:%S.000Z')  + datetime.timedelta(hours=float(offset))
                         except:
                             return True
                     elif trakt_match.type == 'episode':
-                        return datetime.datetime.utcnow() > datetime.datetime.strptime(trakt_match.first_aired,'%Y-%m-%dT%H:%M:%S.000Z')
+                        return datetime.datetime.utcnow() > datetime.datetime.strptime(trakt_match.first_aired,'%Y-%m-%dT%H:%M:%S.000Z')  + datetime.timedelta(hours=float(offset))
                 except:
                     return False
         try:
