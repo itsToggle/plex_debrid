8458db7b3ec3f0059f0be53c7d08b4eede7d8365
itsToggle <71379623+itsToggle@users.noreply.github.com>
2023-02-05 09:17:21 +0100

    new release printing

--- releases/__init__.py
+++ releases/__init__.py
@@ -1421,14 +1421,17 @@ class torrent2magnet:
                 + '&dn=' + metadata[b'info'][b'name'].decode() \
                 + '&tr=' + metadata[b'announce'].decode() 
 
-def print_releases(scraped_releases):
+def print_releases(scraped_releases,uiprint=False):
     longest_file = 0
     longest_cached = 0
     longest_title = 0
     longest_size = 0
+    longest_bitrate = 0
     longest_index = 0
     longest_seeders = 0
     for index, release in enumerate(scraped_releases):
+        if hasattr(release,"bitrate"):
+            release.printbit = str(round(release.bitrate, 2))
         release.printsize = str(round(release.size, 2))
         release.file = '+' + str(release.wanted) + '/-' + str(release.unwanted)
         if len(release.file) > longest_file:
@@ -1439,16 +1442,22 @@ def print_releases(scraped_releases):
             longest_title = len(release.title)
         if len(str(release.printsize)) > longest_size:
             longest_size = len(str(release.printsize))
+        if hasattr(release,"bitrate") and len(str(release.printbit)) > longest_bitrate:
+            longest_bitrate = len(str(release.printbit))
         if len(str(release.seeders)) > longest_seeders:
             longest_seeders = len(str(release.seeders))
         if len(str(index + 1)) > longest_index:
             longest_index = len(str(index + 1))
     for index, release in enumerate(scraped_releases):
-        print(str(index + 1) + ") " + ' ' * (
-                    longest_index - len(str(index + 1))) + "title: " + release.title + ' ' * (
-                            longest_title - len(release.title)) + " | size: " + str(release.printsize) + ' ' * (
-                            longest_size - len(str(release.printsize))) + " | cached: " + '/'.join(
-            release.cached) + ' ' * (longest_cached - len('/'.join(release.cached))) + " | seeders: " + str(
-            release.seeders) + ' ' * (
-                            longest_seeders - len(str(release.seeders))) + " | files: " + release.file + ' ' * (
-                            longest_file - len(release.file)) + " | source: " + release.source)
+        i = str(index + 1) + ") " + ' ' * (longest_index - len(str(index + 1))) 
+        title = "title: " + release.title + ' ' * (longest_title - len(release.title)) 
+        size = " | size: " + str(release.printsize) + ' ' * (longest_size - len(str(release.printsize)))
+        bitrate = " | bitrate: " + str(release.printbit) + ' ' * (longest_bitrate - len(str(release.printbit))) if hasattr(release,"bitrate") else ""
+        cached = " | cached: " + '/'.join(release.cached) + ' ' * (longest_cached - len('/'.join(release.cached)))
+        seeders = " | seeders: " + str(release.seeders) + ' ' * (longest_seeders - len(str(release.seeders)))
+        files = " | files: " + release.file + ' ' * (longest_file - len(release.file)) 
+        source = " | source: " + release.source
+        if uiprint:
+            ui_print(i + title + size + bitrate + cached + seeders + files + source, ui_settings.debug)
+        else:
+            print(i + title + size + bitrate + cached + seeders + files + source)
