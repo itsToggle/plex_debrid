0d110fb139e08f17ca5a5efc58dce03c818f907a
itsToggle <71379623+itsToggle@users.noreply.github.com>
2023-02-17 22:07:19 +0100

    add nyaa proxy setting

--- settings/__init__.py
+++ settings/__init__.py
@@ -378,6 +378,7 @@ settings_list = [
         setting('Orionoid Scraper Parameters',['Please enter a valid orionoid parameter: ','Please enter a valid value: '],scraper.services.orionoid, 'default_opts', entry="parameter", help='This settings lets you control the orionoid scraping parameters. Check out the possible parameters and their values at "https://panel.orionoid.com/" in the "Developers" menu, section "API Docs" under "Stream API".', hidden=True),
         setting('Nyaa parameters', 'Enter custom url parameters. Categories: anime "&c=1_0", anime raw "&c=1_4", anime EN subs "&c=1_2", anime non-EN subs "&c=1_3". Enter your parameters: ',scraper.services.nyaa, 'params', hidden=True),
         setting('Nyaa sleep time', 'Enter a time in seconds to sleep between requests: ',scraper.services.nyaa, 'sleep', hidden=True),
+        setting('Nyaa proxy', 'Enter a proxy to use for nyaa: ',scraper.services.nyaa, 'proxy', hidden=True),
     ]
         ],
     ['Debrid Services', [
