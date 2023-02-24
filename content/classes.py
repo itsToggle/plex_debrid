b5a812693b8291e9026525aabccf21c552d4290b
itsToggle <71379623+itsToggle@users.noreply.github.com>
2023-02-24 21:24:09 +0100

    remove movie year from query if character replacement would remove it

--- content/classes.py
+++ content/classes.py
@@ -390,8 +390,11 @@ class media:
             elif self.type == 'episode':
                 title = releases.rename(self.grandparentTitle)
         if self.type == 'movie':
-            title = title.replace('.' + str(self.year), '')
-            return title + '.' + str(self.year)
+            if regex.search(str(self.year),releases.rename(self.title.replace(str(self.year),'') + ' ' + str(self.year))):
+                title = title.replace('.' + str(self.year), '')
+                return title + '.' + str(self.year)
+            else:
+                return title
         elif self.type == 'show':
             title = title.replace('.' + str(self.year), '')
             return title
@@ -542,10 +545,14 @@ class media:
                     title = releases.rename(self.grandparentTitle)
             title = title.replace('[','\[').replace(']','\]')
             if self.type == 'movie':
-                title = title.replace('.' + str(self.year), '')
-                if year != "":
-                    return '[^A-Za-z0-9]*(' + title + ':?.)\(?\[?(' + str(year) + ')'
-                return '[^A-Za-z0-9]*(' + title + ':?.)\(?\[?(' + str(self.year) + '|' + str(self.year - 1) + '|' + str(self.year + 1) + ')'
+                if regex.search(str(self.year),releases.rename(self.title.replace(str(self.year),'') + ' ' + str(self.year))):
+                    title = title.replace('.' + str(self.year), '')
+                    if year != "":
+                        return '[^A-Za-z0-9]*(' + title + ':?.)\(?\[?(' + str(year) + ')'
+                    return '[^A-Za-z0-9]*(' + title + ':?.)\(?\[?(' + str(self.year) + '|' + str(self.year - 1) + '|' + str(self.year + 1) + ')'
+                else:
+                    title = title.replace('.' + str(self.year), '')
+                    return '[^A-Za-z0-9]*(' + title + ')'
             elif self.type == 'show':
                 title = title.replace('.' + str(self.year), '')
                 return '[^A-Za-z0-9]*(' + title + ':?.)(series.)?((\(?' + str(self.year) + '\)?.)|(complete.)|(seasons?.[0-9]+.[0-9]?[0-9]?.?)|(S[0-9]+.S?[0-9]?[0-9]?.?)|(S[0-9]+E[0-9]+))'
