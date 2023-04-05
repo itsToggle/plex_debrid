e1b2c2385f2c4092182e17341c6be4c68060f21a
itsToggle <71379623+itsToggle@users.noreply.github.com>
2023-04-05 18:02:47 +0200

    add torrentio as scraper source

--- scraper/services/__init__.py
+++ scraper/services/__init__.py
@@ -6,10 +6,11 @@ from scraper.services import jackett
 from scraper.services import prowlarr
 from scraper.services import orionoid
 from scraper.services import nyaa
+from scraper.services import torrentio
 
 #define subclass method
 def __subclasses__():
-    return [rarbg,x1337,jackett,prowlarr,orionoid,nyaa]
+    return [rarbg,x1337,jackett,prowlarr,orionoid,nyaa,torrentio]
 
 active = ['rarbg', '1337x', ]
 overwrite = []
